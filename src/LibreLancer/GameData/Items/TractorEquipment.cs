3c686c10ae0ec1bd8e1d6131d2195d45f0387edc
Schneider <jacobhhagel@gmail.com>
2024-08-17 15:01:15 +0200

    enable launching with tractor

--- src/LibreLancer/GameData/Items/TractorEquipment.cs
+++ src/LibreLancer/GameData/Items/TractorEquipment.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibreLancer.Sounds;
+using LibreLancer.World;
 
 namespace LibreLancer.GameData.Items
 {
     public class TractorEquipment : Equipment
     {
         public Data.Equipment.Tractor Def;
+        static TractorEquipment() => EquipmentObjectManager.RegisterType<TractorEquipment>(AddEquipment);
+        static GameObject AddEquipment(GameObject parent, ResourceManager res, SoundManager snd, EquipmentType type, string hardpoint, Equipment equip)
+        {
+            //just a dummy to prevent an error here.
+            return new GameObject();
+        }
     }
 }
